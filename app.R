ee44debe64ab176df6568f1620cb10ab8f867f00
aridf <adecterfrain@gmail.com>
2020-10-06 12:35:59 -0500

    update visuals

--- app.R
+++ app.R
@@ -1,33 +1,57 @@
 suppressPackageStartupMessages({
   library(shiny)
   library(tidyverse)
+  library(shinythemes)
 })
 
 data <- readRDS("data/long_data.RDS")
 group_lookup <- read_csv("data/groups.csv")
 vars <- read_csv("data/vars.csv")
 
-ui <- fluidPage(
+ui <- fluidPage(theme = shinytheme("slate"),
+  titlePanel(
+    h1("Title", align = "center")
+  ),
   fluidRow(
-    column(6, selectInput("dio", "Diocese", data$diocese, data$diocese)),
-    column(
-      6, 
+    column(2, offset = 1, img(src = "logo.png", height = "200px")),
+    column(8, offset = -1, 
+           h3("Instructions"),
+           p(style="text-align: justify;",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+             eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+             Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+             laboris nisi ut aliquip ex ea commodo consequat. 
+             Duis aute irure dolor in reprehenderit in voluptate velit esse 
+             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+             cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+             anim id est laborum."
+           )
+          )
+  ),
+  fluidRow(
+    column(4, offset = 2, selectInput("dio", "Diocese", data$diocese, data$diocese)),
+    column(4, offset = -2,
       selectInput(
         "group", 
         "Group",
-        setNames(group_lookup$group_id, group_lookup$name),
+        setNames(group_lookup$group_id, group_lookup$group_name),
         multiple = TRUE
       )
     )
   ),
   fluidRow(
+    br(),
     column(12, tableOutput("table"), align = "center")
   )
 )
 
 server <- function(input, output, session) {
   
-  selected <- reactive(
+  selected <- reactive({
+    if (input$dio == "" || nrow(data %>% filter(group_id %in% input$group)) == 0) {
+      return(NULL)
+    }
+    
     data %>% 
       filter(diocese == input$dio) %>%
       filter(group_id %in% input$group) %>%
@@ -45,7 +69,7 @@ server <- function(input, output, session) {
         values_from = Count
       ) %>% 
       select(-diocese, -var)
-  )
+  })
   
   output$table <- renderTable(
     {
