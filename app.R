ee71fd015d4c07767a96accfcad5ee8b41848840
aridf <adecterfrain@gmail.com>
2021-02-10 08:46:49 -0500

    add favicon

--- app.R
+++ app.R
@@ -16,6 +16,12 @@ ui <- fluidPage(
   
   # add custom css
   tags$head(
+    tags$head(
+      tags$link(rel = "shortcut icon", href = "favicon.ico"),
+      tags$link(rel = "apple-touch-icon", sizes = "180x180", href = "favicon.ico"),
+      tags$link(rel = "icon", type = "image/png", sizes = "32x32", href = "/favicon-32x32.png"),
+      tags$link(rel = "icon", type = "image/png", sizes = "16x16", href = "/favicon-16x16.png")
+    ),
     tags$title("CMS Pastoral Care Database"),
     tags$style(HTML("
       tr:first-child { 
